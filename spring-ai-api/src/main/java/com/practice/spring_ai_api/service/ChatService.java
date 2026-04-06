661b516372f8a488ef9e8dd5d3bb7b5ea51ed8ba
Magnum Adelano <magnumcs@outlook.com>
2026-04-06 00:36:54 -0300

    Adicionando integração com o ollama.

--- spring-ai-api/src/main/java/com/practice/spring_ai_api/service/ChatService.java
+++ spring-ai-api/src/main/java/com/practice/spring_ai_api/service/ChatService.java
@@ -2,14 +2,21 @@ package com.practice.spring_ai_api.service;
 
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class ChatService {
 
     private final ChatModel chatModel;
+
+    @Value(value = "${spring.ai.openai.chat.model}")
+    private String model;
 
     public ChatService(ChatModel chatModel) {
         this.chatModel = chatModel;
@@ -23,11 +30,11 @@ public class ChatService {
         ChatResponse response = chatModel.call(
                 new Prompt(
                         prompt,
-                        OpenAiChatOptions.builder()
-                                .model("gpt-4o")
+                        ChatOptions.builder()
+                                .model(model)
                                 .temperature(0.4)
                                 .build()
                 ));
-        return response.getResult().getOutput().getText();
+        return Objects.requireNonNull(response.getResult()).getOutput().getText();
     }
 }
